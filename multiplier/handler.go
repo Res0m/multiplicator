0fbddeb66f59c375bee318c6dd2c382a73032e5c
Anton Remzov <ant.remzov@yandex.ru>
2025-09-11 12:41:05 +0300

    round func

--- multiplier/handler.go
+++ multiplier/handler.go
@@ -2,7 +2,9 @@ package multiplier
 
 import (
 	"encoding/json"
+	"math"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -14,9 +16,14 @@ func NewMultiplierHandler(router *mux.Router, handler *MultiplierHandler) {
 	router.HandleFunc("/get", handler.GetMultiplier).Methods("GET")
 }
 
+func roundToDecimal(value float64, decimalPlaces int) float64 {
+	multiplier := math.Pow(10, float64(decimalPlaces))
+	return math.Round(value*multiplier) / multiplier
+}
+
 func (handler *MultiplierHandler) GetMultiplier(w http.ResponseWriter, r *http.Request) {
 	var response Result
-	multipl := handler.MultiplierService.GenerateMultiplierPareto(1.0, 10000.0)
+	multipl := roundToDecimal(handler.MultiplierService.GenerateMultiplierPareto(1.0, 10000.0), 2)
 	response = Result{
 		Result: multipl,
 	}
